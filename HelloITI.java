e7c4df275d56e3a5e4589c22939c97dc37167e77
abdomake73 <77170182+abdomake73@users.noreply.github.com>
2022-07-29 14:01:22 +0200

    1th add hellworld

--- HelloITI.java
+++
@@ -1,7 +0,0 @@
-class HelloITI
-{
-	public static void main (String[] args)
-	{
-		System.out.println("HelloITI");
-	}
-}
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,4 @@
 class HelloWorld
 {
-	public static void main (String[] args)
-	{
-		System.out.println("HelloWorld");
-	}
+	public static void main 
 }
